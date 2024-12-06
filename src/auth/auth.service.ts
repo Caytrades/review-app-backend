d40e736e8c43f9b44034ec813b94a628138e1f12
Caytrades <nanakwameopokuababio@gmail.com>
2024-12-06 17:28:17 +0000

    Auth Services Created

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { AdminService } from '../admin/admin.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly adminService: AdminService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async validateAdmin(username: string, password: string): Promise<any> {
+    const admin = await this.adminService.findByUsername(username);
+    if (admin && (await bcrypt.compare(password, admin.password))) {
+      const { password, ...result } = admin; // Exclude the password from the returned result
+      return result;
+    }
+    throw new UnauthorizedException('Invalid credentials');
+  }
+
+  async login(admin: any) {
+    const payload = { username: admin.username, sub: admin.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
