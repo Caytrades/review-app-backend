80c3fed8c6504a6ec905efdc3f980356c4a68e67
Caytrades <nanakwameopokuababio@gmail.com>
2024-12-11 17:12:55 +0000

    Questions Module Added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Feedback } from './feedback/feedback.entity';
-import { Admin } from './admin/admin.entity';
+import { Feedback } from './entities/feedback.entity';
+import { Admin } from './entities/admin.entity';
 import { FeedbackModule } from './feedback/feedback.module';
 import { AdminModule } from './admin/admin.module';
 import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
+import { Product } from './entities/product.entity';
+import { UsersModule } from './users/users.module';
+import { QuestionsModule } from './questions/questions.module';
 import * as dotenv from 'dotenv';
 
 dotenv.config()
@@ -18,12 +22,15 @@ dotenv.config()
       username:  process.env.DB_USERNAME,
       password:  process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [Feedback, Admin],
+      entities: [Feedback, Admin, Product],
       synchronize: true,
     }),
     FeedbackModule,
     AdminModule,
     AuthModule,
+    ProductModule,
+    UsersModule,
+    QuestionsModule,
   ],
 })
 export class AppModule {}
