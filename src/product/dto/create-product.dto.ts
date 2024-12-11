1db02991f41bbfe86338733814e7c05352f94dfe
Caytrades <nanakwameopokuababio@gmail.com>
2024-12-11 17:16:11 +0000

    Product DTO

---
+++ src/product/dto/create-product.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  company: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+}
