fa9cef47c6e5718f1a2c71989c6ef9a188b8fd7e
Caytrades <nanakwameopokuababio@gmail.com>
2024-12-11 17:20:11 +0000

    Refactoring Of Controllers

--- src/feedback/feedback.controller.ts
+++ src/feedback/feedback.controller.ts
@@ -1,24 +1,34 @@
 import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
 import { FeedbackService } from './feedback.service';
-import { AuthGuard } from '@nestjs/passport';
+// import { AuthGuard } from '@nestjs/passport';
+import { CreateFeedbackDto } from './dto/create-feedback.dto';
 
 @Controller('feedback')
 export class FeedbackController {
     constructor(private readonly feedbackService: FeedbackService) {}
 
     // @UseGuards(AuthGuard('jwt'))
-    @Get()
-    async listFeedbacks() {
-        return this.feedbackService.findAll();
-    }
-
-    @Get('/:id')
-    getFeedback () {
-
-    }
-
     @Post()
-    async createMessage(@Body('message') message: string) {
-        return this.feedbackService.create(message);
+    create(@Body() createFeedbackDto: CreateFeedbackDto) {
+      return this.feedbackService.create(createFeedbackDto);
     }
+  
+    @Get('/product/:id')
+    async getFeedbacksByProduct(@Param('productId') productId: string) {
+        return this.feedbackService.findByProduct(+productId); // Convert productId to a number
+    }
+
+    // @Get()
+    // async listFeedbacks() {
+    //     return this.feedbackService.findAll();
+    // }
+
+    // @Get('/:id')
+    // getFeedback () {
+
+    // }
+
+    // @Post()
+    // async createMessage(@Body('message') message: string) {
+    //     return this.feedbackService.create(message);
 }
